d47bd2f7672f8f2746f8111c5114173a663fd9d5
rotarymars <sgoto2050@gmail.com>
2024-06-06 14:08:52 +0900

    commit

--- generate_primenumber.py
+++ generate_primenumber.py
@@ -2,7 +2,13 @@ import math
 a=int(input("bigger than:"))
 b=int(input("less than:"))
 m=set()
+f = open("./GeneratedPrime.txt","rb")
+for i in f:
+    m.add(int(i.rstrip()))
+f.close()
+print(m)
 def isprime(a: int) -> bool: 
+    global m
     biggestprimeinm=2
     for i in m:
         if a <= i:
@@ -18,6 +24,11 @@ def isprime(a: int) -> bool:
     m.add(a)
     return True
 for i in range(a,b+1):
-    if isprime(i):
-        print(i)
+    isprime(i)
+m=sorted(m)
+f = open("./GeneratedPrime.txt",'w')
+for i in m:
+    f.write(str(i)+'\n')
+#f.writelines(m)
+f.close()
 print(m)

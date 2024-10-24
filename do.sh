89bb1f5e59c5093075276eda5e910d5346f04199
rotarymars <s.goto2050@gmail.com>
2024-10-25 07:03:50 +0900

    commit

--- do.sh
+++ do.sh
@@ -1,5 +1,9 @@
-for (( ; ; )); do
+for ((i = 0; ; i++)); do
   ./a.out
   git add .
   git commit -m "commit"
+  # every 50 time, push the commit
+  if [[ $((i % 50)) -eq 0 ]]; then
+    git push
+  fi
 done
